948ea70478ed0214e4916c520441ceed4dc06853
Giancarlo Vigneri <vignericodes@gmail.com>
2023-08-19 17:11:42 -0700

    refactor to use f strings

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # print("Welcome to my amazing band name generator.")
 # city = input("What city where you born in?\n")
 # pet = input("What is the name of your childhood pet?\n")
-# print("Your band name could be " + city + " " + pet + "!")
+# print(f"Your band name could be {city} {pet}!")
 
 # ------------------
 
@@ -16,13 +16,13 @@
 # diners = int(input("How many people split the bill? "))
 # tipPercent = 100 + int(input(
 #     "What percentage of tip would you like to give 15, 20, or 25? "))
-# result = (billTotal * tipPercent/diners)/100
-# print("Each person should pay $" + str(result))
+# result = round((billTotal * tipPercent/diners)/100, 2)
+# print(f"Each person should pay ${str(result)}")
 
 # ------------------
 
-print("Welcome to the BMI calculator.")
-height = float(input("Enter your height(m) "))
-weight = float(input("Enter your weight(kg) "))
-bmiStr = str(round(weight / height ** 2, 1))
-print(bmiStr + " is your BMI.")
+# print("Welcome to the BMI calculator.")
+# height = float(input("Enter your height(m) "))
+# weight = float(input("Enter your weight(kg) "))
+# bmiStr = str(round(weight / height ** 2, 1))
+# print(f"{bmiStr} is your BMI.")
